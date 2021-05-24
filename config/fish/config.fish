1009aa54538cdf38a98b20e93ae774c58a4b94a9
theissn <theissnielsen@gmail.com>
2021-05-24 18:14:42 +0100

    updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,13 @@
 alias weather="curl http://wttr.in/LCY"
+alias z="zathura"
 
 set fish_greeting
 
 set -gx EDITOR vim
+set -gx BROWSER firefox-developer-edition
+set -gx PATH "$HOME/.local/bin" $PATH
+set -gx PATH "$HOME/.local/bin/sb" $PATH
+
+for x in docker docker-compose pacman;
+	alias $x="sudo $x"
+end
