edeeeaff2c8d6454d9c88002d924cd1a0aac79e2
theissn <theissnielsen@gmail.com>
2021-05-29 09:07:43 +0100

    sync, fish and pipewire

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,7 +7,12 @@ set -gx EDITOR vim
 set -gx BROWSER firefox-developer-edition
 set -gx PATH "$HOME/.local/bin" $PATH
 set -gx PATH "$HOME/.local/bin/sb" $PATH
+set -gx XDG_CONFIG_HOME "$HOME/.config"
 
 for x in docker docker-compose pacman;
 	alias $x="sudo $x"
+end
+
+if type -q nvim 
+   alias vim="nvim" 
 end
